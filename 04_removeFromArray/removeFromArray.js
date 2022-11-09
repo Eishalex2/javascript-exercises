10c6152bd1886f2630a6c9b9ff1d7722a5df9d3f
Eishalex2 <t.whitney.a@gmail.com>
2022-11-09 10:57:02 -0800

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, remove, ...removeMore) {
+  const initialFilter = array.filter((item) => item !== remove);
+  let finalFilter = initialFilter;
+  for (const item of removeMore) {
+    let index = finalFilter.indexOf(item);
+    if (index > -1) {
+      finalFilter.splice(index, 1);
+    }
+  }
+  return finalFilter;
 };
 
 // Do not edit below this line
