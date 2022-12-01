0324f0a850c5627423d311d38c2bf7f514341c0b
Eishalex2 <t.whitney.a@gmail.com>
2022-12-01 11:55:58 -0800

    Finish exercise 8, calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, number) => {
+    return total + number;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, number) => {
+    return total * number;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+	return base ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number === 0) return 1;
+  let total = 1;
+  for (let i = 1; i <= number; i++) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
