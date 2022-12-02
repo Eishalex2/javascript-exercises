391a32dd39d1afe96e8c0c9fdd71163207879a1d
Eishalex2 <t.whitney.a@gmail.com>
2022-12-01 17:28:54 -0800

    Finish exercise 10, fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+//index starts from 1;
+const fibonacci = function(index) {
+  let fibArray = [1, 1];
+  if (index < 0) return "OOPS";
+  for (i = 2; i < index; i++) {
+    fibArray[i] = fibArray[i-1] + fibArray[i-2]; 
+  }
+  return fibArray[fibArray.length-1];
 };
 
 // Do not edit below this line
