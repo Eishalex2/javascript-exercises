face866853714ea1885b33fe00fe1134ab88c471
Eishalex2 <t.whitney.a@gmail.com>
2022-12-02 14:44:26 -0800

    Finish exercise 12, findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,10 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(people) {
+  const thisYear = new Date().getFullYear();
+  const ordered = people.sort((a, b) => 
+    ((a.yearOfDeath || thisYear) - a.yearOfBirth) > 
+    ((b.yearOfDeath || thisYear) - b.yearOfBirth) ? -1 : 1);
+  return ordered[0];
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
